11030b23d0d00ccc27e85f4ec8e30e231298e25a
Ramil Ashrafulin <ramilashrafulinfrontend@gmail.com>
2023-02-16 14:15:10 +0300

    feat: lab02

---
+++ lab02/lab02.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+   <title>
+      Тестируем PHP
+   </title>
+   <?php echo "<link rel='stylesheet' href='style.css'>"; ?>
+</head>
+
+<body>
+   <header>
+      <?php echo "<img src='./mospolytech-logo-white.svg'>" ?>
+      <?php echo "<p class='text'>Домашняя работа</p>" ?>
+   </header>
+   <main>
+      <?php echo "<p class='text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius rerum mollitia illo quidem quis. Voluptates iusto
+      eveniet delectus at neque, ipsa obcaecati reprehenderit modi quibusdam in quisquam consectetur accusantium
+      quaerat.</p>" ?>
+   </main>
+   <footer>
+      <ul class="ul">
+         <li class="li"><a href="https://vk.com/tataring_gmp" target="blank">ВКонтакте</a></li>
+         <li class="li"><a href="https://t.me/ramil_ashrafulin" target="blank">Telegram</a></li>
+         <li class="li"><a href="https://www.linkedin.com/in/ramil-ashrafulin-a0810b234" target="blank">LinkedIn</a></li>
+      </ul>
+   </footer>
+</body>
+
+</html>
